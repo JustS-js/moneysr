305c0a69d615756bffbbc043c3df5b3e31edbf4a
Sam <74768376+JustS-js@users.noreply.github.com>
2025-03-25 14:34:10 +0300

    Update and rename ExampleMod.java to SRMod.java

--- src/main/java/net/just_s/SRMod.java
+++ src/main/java/net/just_s/SRMod.java
@@ -1,12 +1,12 @@
-package com.example;
+package net.just_s;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class SRMod implements ModInitializer {
+	public static final String MOD_ID = "moneysr";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
