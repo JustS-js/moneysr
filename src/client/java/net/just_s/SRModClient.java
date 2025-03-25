2cf18c4ce7a076a29d374ba63aeb423bd245ee33
Sam <74768376+JustS-js@users.noreply.github.com>
2025-03-25 14:36:42 +0300

    Update and rename ExampleModClient.java to SRModClient.java

--- src/client/java/net/just_s/SRModClient.java
+++ src/client/java/net/just_s/SRModClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package net.just_s;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class SRModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
